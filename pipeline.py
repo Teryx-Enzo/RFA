686452cebaf0b80e3eb4932913f16fcb93b83920
ewenorsal.1 <ewenorsal.1@gmail.com>
2024-04-14 16:35:55 -0400

    Refactor MSE

--- pipeline.py
+++ pipeline.py
@@ -5,8 +5,8 @@ from torch.utils.data import DataLoader
 from torch.autograd import Variable
 import torch.nn.functional as F
 import torch
-from lib import FA
-from lib import backprop
+import FA
+import backprop
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -14,20 +14,17 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('device =',device)
 
 
-def train_model(model, optimizer, train_loader, num_epochs, criterion):
+def train_model(model, optimizer, train_loader, num_epochs, criterion, batchSize=32):
     logName = f"{model.__class__.__name__}_{model.num_layers}"
     print("Training", logName)
     logger_train = open(f"logs/{logName}.txt", 'w')
     for epoch in range(1, num_epochs+1):
         for idx_batch, (inputs, targets) in enumerate(train_loader):
-            # flatten the inputs from square image to 1d vector
-            inputs = inputs.view(BATCH_SIZE, -1)
-            # wrap them into varaibles
-            inputs, targets = Variable(inputs), Variable(targets)
-            # get outputs from the model
+            targetVectors = F.one_hot(targets, 10)
+            inputs = inputs.view(batchSize, -1)
+            inputs, targetVectors = Variable(inputs), Variable(targetVectors)
             outputs = model(inputs.to(device))
-            loss = criterion(outputs, targets.to(device))
-
+            loss = criterion(outputs, targetVectors.to(device).float())
             model.zero_grad()
             loss.backward()
             optimizer.step()
@@ -46,30 +43,19 @@ def test_model(model, test_loader, criterion):
     total = 0
     with torch.no_grad():
         for data, targets in test_loader:
-            # Flatten the inputs from square image to 1d vector
+            targetVectors = F.one_hot(targets, 10)
             inputs = data.view(data.size(0), -1)
-            # Wrap inputs and targets into variables
-            inputs, targets = Variable(inputs), Variable(targets)
-            # Get outputs from the model
+            inputs, targetVectors, targets = Variable(inputs), Variable(targetVectors), Variable(targets)
             outputs = model(inputs.to(device))
-            # Calculate loss
-            loss = criterion(outputs, targets.to(device))
-            # Sum up batch loss
+            loss = criterion(outputs, targetVectors.to(device).float())
             test_loss += loss.item() * inputs.size(0)
-            # Get the predicted labels
-            _, predicted = torch.max(outputs.data, 1)
-            # Total number of samples
             total += targets.size(0)
-            # Number of correct predictions
-            correct += (predicted == targets.to(device)).sum().item()
+            correct += (outputs.argmax(dim=1) == targets.to(device)).sum().item()
 
-    # Average test loss
     test_loss = test_loss / total
-    # Accuracy
     accuracy = 100 * correct / total
-
     print('Test Loss: {:.4f}'.format(test_loss))
-    print('Accuracy: {:.2f}%'.format(accuracy))
+    print('Accuracy: {:.2f}%'.format(accuracy)) 
 
 
 
@@ -88,18 +74,17 @@ if __name__ == '__main__':
                                             ])),
                             batch_size=BATCH_SIZE, shuffle=True)
     loss_crossentropy = torch.nn.CrossEntropyLoss()
-
-    loss_mse = torch.nn.MSELoss
+    loss_mse = torch.nn.MSELoss()
     
     model_fa = FA.FANetwork(in_features=784, num_layers=4, num_hidden_list=[1000, 30, 20, 10], activation_function=F.tanh).to(device)
     optimizer_fa = torch.optim.SGD(model_fa.parameters(),lr=1e-4, momentum=0.9, weight_decay=0.001, nesterov=True)
-    train_model(model_fa, optimizer_fa,train_loader, num_epochs=50, criterion=loss_crossentropy)
+    train_model(model_fa, optimizer_fa,train_loader, num_epochs=50, criterion=loss_mse, batchSize=BATCH_SIZE)
     print("Testing Feedforward DFA Model...")
     test_model(model_fa, test_loader, loss_mse)
 
     model_bp = backprop.BackPropNetwork(in_features=784, num_layers=4, num_hidden_list=[1000, 30, 20, 10]).to(device)
     optimizer_bp = torch.optim.SGD(model_bp.parameters(),lr=1e-4, momentum=0.9, weight_decay=0.001, nesterov=True)
-    train_model(model_bp, optimizer_bp, train_loader, num_epochs=50, criterion=loss_crossentropy)
+    train_model(model_bp, optimizer_bp, train_loader, num_epochs=50, criterion=loss_mse, batchSize=BATCH_SIZE)
     print("Testing BackProp Model...")
     test_model(model_bp, test_loader, loss_mse)
     
